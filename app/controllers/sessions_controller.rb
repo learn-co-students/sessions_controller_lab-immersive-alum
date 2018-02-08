cd343c9fb9a617fc38ad67ed0a6c74816d41fe9a
TJ Carney <tjcarney@stny.rr.com>
2018-02-08 13:26:24 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    if params[:name] == "" || params[:name] == nil
+      redirect_to login_path
+    else
+      session[:name] = params[:name]
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    if session[:name]
+      session.delete :name 
+    end
+    redirect_to root_path
+  end
+
+end
